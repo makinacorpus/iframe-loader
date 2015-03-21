075d445cbd8c5199610412923e2bd0cc0626b400
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2015-03-21 10:50:27 +0100

    Add base index table & dataset polyfill

--- src/scripts/modules/main-app.js
+++ src/scripts/modules/main-app.js
@@ -3,9 +3,25 @@ module.exports = (function () {
 		phSelector: '.mkc-placeholder'
 	};
 
+	require('./dataset-polyfill.js');
 	var pym          = require('pym.js');
 	var randomstring = require('randomstring');
 
+	function _getURL (dataIndex) {
+		/**
+		 * TODO: Outsource targets table
+		 */
+		var index = {
+			'2015-cantons-et-candidats': {
+				url: 'http://makinacorpus.github.io/elections/departementales-2015/app/public/'
+			},
+			'2015-resultats-departementales': {
+				url: 'http://makinacorpus.github.io/elections/departementales-2015/app/public/departement.html'
+			}
+		}
+		var result = index[dataIndex];
+		return (result && result.url) ? result.url : 'about:blank';
+	}
 
 	function _browserContext () {
 		return (typeof window !== 'undefined');
@@ -18,13 +34,14 @@ module.exports = (function () {
 	function _eachPlaceholder (placeholder) {
 		var newID      = 'mkc-' + randomstring.generate(8);
 		placeholder.id = newID;
+		var dataIndex  = placeholder.dataset.source;
 
-		var pymParent = new pym.Parent(newID, 'http://www.perdu.com', {});
+		var pymParent  = new pym.Parent(newID, _getURL(dataIndex), {});
+
+		pymParent.sendMessage('dataset', JSON.stringify(placeholder.dataset));
 	}
 
 	function _init () {
-		console.log(_browserContext() ? 'Browser !' : 'Not in browser :(');
-
 		var placeholders = _findPlaceholders();
 		[].forEach.call(placeholders, _eachPlaceholder);
 	}
